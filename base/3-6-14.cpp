94c12e6c6c818657f74c39881f9a57887997a86c
Alex <66781621+AlexTowny@users.noreply.github.com>
2021-10-14 23:03:55 +0400

    Create 3-6-14.cpp

---
+++ base/3-6-14.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+double func(double x){
+	if(x<0)
+		return 0.2*pow(x,2) - x - 0.1;
+	else if(x == 0.1)
+		return 0;
+	else
+		return pow(x,2)/(x - 0.1);
+}
+
+int main() {
+
+  double a,b,h;
+  cout.precision(9);
+
+  cout << "Please, enter a,b,h value" << '\n';
+  cin >> a >> b >> h;
+  cout << 'x' << " | " << "f(x)" << '\n';
+  cout << "-------------------------\n";
+  for (double i = a; i <= b; i+=h){
+	  cout << fixed << i << " | " << func(i) << '\n';
+  }
+  cout << "-------------------------";
+  return 0;
+}
