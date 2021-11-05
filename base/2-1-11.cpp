558c0bd735ff6ebe0bf00f28ac2db3f665e257cc
Alex <66781621+AlexTowny@users.noreply.github.com>
2021-11-05 10:46:34 +0400

    Update 2-1-11.cpp

--- base/2-1-11.cpp
+++ base/2-1-11.cpp
@@ -9,17 +9,18 @@
 #include <iomanip>
 #include <cmath>
 using namespace std;
-int func(int arg) {
+int func(int &arg) {
     
     if (arg % 5 == 0)
-        return arg / 5;
+        arg /= 5;
     else
-        return arg + 1;
+        arg += 1;
 }    
 int main() {
     
     int inp;
     cin >> inp;
-    cout << func(inp);
+    func(inp);
+    cout <<  inp;
     return 0;
 }
