e2e0a7e3c71db927aa91ed947b8b00dcbc5d60b7
Alex <66781621+AlexTowny@users.noreply.github.com>
2021-11-05 11:14:06 +0400

    Update 4-1-10.cpp

--- base/4-1-10.cpp
+++ base/4-1-10.cpp
@@ -10,13 +10,21 @@ int main() {
   int n;
   cin>>n;
 
-  int a[1000000];
-  a[0] = 2;
-  a[1] = 4;
 
-  for(int i=2;i<n;i++){
-	a[i] = 6*a[i-1] - a[i-2];
+  int a = 2;
+  int b = 4;
+  int c;
+
+  for(int i=1;i<n+1;i++){
+	 if (i > 2){
+	 c = b;
+	 b = 6*b-a;
+	 a = c;
+	 cout << "b"<< i << " = " <<   b << '\n';
+	 }
+	 else
+		 cout << "b"<< i << " = " <<  ((i==1)?(a):(b)) << '\n';
   }
-  cout << a[n-1];
+  //cout << ((n>1)?(b):(a));
   return 0;
 }
