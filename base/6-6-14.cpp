2464ffad526756571754774c8cd41b534bfaddb1
Alex <66781621+AlexTowny@users.noreply.github.com>
2021-12-08 02:48:33 +0400

    Create 6-6-14.cpp

---
+++ base/6-6-14.cpp
@@ -0,0 +1,86 @@
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+
+using namespace std;
+
+void delate_row(int** &a, int &n, int idx){
+	for (int i = idx; i<n-1; i++)
+		a[i] = a[i+1];
+	n--;
+}
+
+void delate_col(int** &a, int &n, int &m , int idx){
+	for (int j = idx; j<m-1; j++)
+		for (int i = 0; i<n; i++)
+			a[i][j] = a[i][j+1];
+	m--;
+}
+
+
+int main() {
+
+	int n,m,k,g;
+	cin >> n >> m;
+	cin >> k >> g;
+	int** a = new int*[n];
+
+	for (int i = 0; i < n; i++)
+		a[i] = new int[m];
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			cin >> a[i][j];
+
+	if(n==m){
+		bool flag = true;
+		for (int i = 0; i < n; i++)
+				flag &= (a[i][k]==a[g][i]);
+		if(flag)
+		{
+			delate_row(a,n,k);
+			delate_col(a,n,m,g);
+		}
+	}
+	else
+		cout << "Can't make compare!\n";
+
+	for (int i = 0; i < n; i++, cout << '\n')
+			for (int j = 0; j < m; j++)
+				cout << a[i][j] <<' ';
+  return 0;
+}
+/*
+4 4
+0 0
+1 2 3 4
+2 3 3 3
+3 3 3 3
+4 4 4 4
+
+3 3 3
+3 3 3
+4 4 4
+----
+3 4
+1 2
+1 1 1 1
+2 3 4 5
+6 7 8 9
+
+Can't make compare!
+1 1 1 1
+2 3 4 5
+6 7 8 9
+----------
+3 3
+1 1
+2 1 4 
+1 1 1
+3 1 5
+
+2 4 
+3 5 
+*/
