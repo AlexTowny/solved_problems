2bd5331061be940d1e09ea51b5cc6b5f3f5efea7
Alex <66781621+AlexTowny@users.noreply.github.com>
2021-12-16 08:46:09 +0400

    Create 2_.cpp

---
+++ seminars/16_12_21_controll/2_.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	int n;
+	cout << "Enter length of array of numbers:";
+	cin >> n;
+	int* a = new int[n];
+	cout << "Enter all elements of array:";
+	for(int i = 0; i< n; i++)
+		cin >> a[i];
+
+	cout << "Indexes of double-digit numbers(Numbering start from 0):";
+	for(int i = 0; i< n; i++)
+		if(a[i]>9 && a[i]<100)
+			cout << i << ' ';
+
+	return 0;
+}
+
+
