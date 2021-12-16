0ddb93def8a261e654011f9e698f4aa310aa8f16
Alex <66781621+AlexTowny@users.noreply.github.com>
2021-12-16 08:38:55 +0400

    Create 1_.cpp

---
+++ seminars/16_12_21_controll/1_.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	int a,b,l,r;
+	int sum_a = 0;
+	cout << "Enter A value:";
+	cin >> a;
+	cout << "Enter B value:";
+	cin >> b;
+
+	l = min(a,b);
+	r = max(a,b);
+
+	for(int i = l; i< r + 1; i++)
+		if(i%2==0 && i%4 != 0)
+			sum_a += i;
+
+	cout << sum_a;
+
+	return 0;
+}
