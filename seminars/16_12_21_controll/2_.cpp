5ea6c936ba10c167ed3612b788c8cf8adc6866b4
Alex <66781621+AlexTowny@users.noreply.github.com>
2021-12-16 09:33:45 +0400

    Update 2_.cpp

--- seminars/16_12_21_controll/2_.cpp
+++ seminars/16_12_21_controll/2_.cpp
@@ -3,18 +3,20 @@
 
 using namespace std;
 
+double const eps = 1e-6;
+
 int main() {
 	int n;
 	cout << "Enter length of array of numbers:";
 	cin >> n;
-	int* a = new int[n];
+	double* a = new double[n];
 	cout << "Enter all elements of array:";
 	for(int i = 0; i< n; i++)
 		cin >> a[i];
 
 	cout << "Indexes of double-digit numbers(Numbering start from 0):";
 	for(int i = 0; i< n; i++)
-		if(abs(a[i])>9 && abs(a[i])<100)
+		if(abs(a[i])>10-eps && abs(a[i])<100-eps)
 			cout << i << ' ';
 	delete [] a;
 	a = nullptr;
